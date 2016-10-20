9893a882793da32e18c42bab9ade14e5635b1be8
markbwarner <markbwarner@gmail.com>
2016-10-20 16:00:59 -0400

    Update JavaFunctions_create2.sql
    remove extra functions

--- JavaFunctions_create2.sql
+++ JavaFunctions_create2.sql
@@ -15,14 +15,11 @@ CREATE LIBRARY JavaScalarFunctions AS :libSfile DEPENDS '/opt/vertica/bin/elasti
 CREATE LIBRARY JavaTransformFunctions AS :libTfile DEPENDS '/opt/vertica/bin/elasticsearch-1.4.4.jar:/opt/vertica/bin/lucene-core-4.10.3.jar:/opt/vertica/bin/lucene-analyzers-common-4.10.3.jar' LANGUAGE 'JAVA';
 -- Step 2: Create Functions
 CREATE FUNCTION add2ints AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.Add2intsInfo' LIBRARY JavaScalarFunctions;
-CREATE FUNCTION ELKadd2ints AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.ELKAdd2intsInfo' LIBRARY JavaScalarFunctions;
-CREATE FUNCTION ELKadd2intsAcct AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.ELKAdd2intsInfoAcct' LIBRARY JavaScalarFunctions;
 CREATE FUNCTION ElasticCount AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.ElasticSearchCount' LIBRARY JavaScalarFunctions;
 CREATE FUNCTION addanyints AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.AddAnyIntsInfo' LIBRARY JavaScalarFunctions;
 
 
 CREATE TRANSFORM FUNCTION topk AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.TopKFactory' LIBRARY JavaTransformFunctions;
-CREATE TRANSFORM FUNCTION ElasticCountT AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.ElasticSearchCountTransform' LIBRARY JavaTransformFunctions;
 CREATE TRANSFORM FUNCTION polytopk  AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.PolyTopKFactory' LIBRARY JavaTransformFunctions;
 CREATE TRANSFORM FUNCTION topkwithparams AS LANGUAGE 'Java' NAME 'com.vertica.JavaLibs.TopKPerPartitionWithParamsFactory' LIBRARY JavaTransformFunctions;
 
